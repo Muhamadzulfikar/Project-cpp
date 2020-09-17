cca750e85d1ad6b3ec18ddb93abbeef605392715
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2020-09-17 21:17:18 +0700

    Add files via upload

---
+++ Program-pembagian/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float nilai1,nilai2;
+	float pembagian;
+	cout<<"masukan nilai1 : ";
+	cin>>nilai1;
+	cout<<"masukan nilai2 : ";
+	cin>>nilai2;
+	pembagian = nilai1/nilai2;
+	cout<<"hasil adalah"<<pembagian<<endl;
+	return 0;
+}
